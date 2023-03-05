2c15ba8c0229a67daaf14fa95bbb2cc07c8fc600
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-05 16:30:59 -0500

    added styling to search input

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -7,12 +7,12 @@ const Searchbar = () => {
   const handleChange = (event) => {
     setSearchValue(event.target.value)
   }
-  return (<div>
+  return (<div className='searchBar'>
     <Link to={`/search/${searchValue}`} state={{searchValue: `${searchValue}`}}>
     <img className='magnifyingGlass' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Magnifying_glass_icon.svg/1200px-Magnifying_glass_icon.svg.png' />
     </Link>
     
-  <input placeholder='Search Albums' onChange={handleChange} />
+  <input className='searchInput'placeholder='Search Albums' onChange={handleChange} />
   </div>)
 }
 export default Searchbar
