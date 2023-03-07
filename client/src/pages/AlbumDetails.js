174d4bf52d4b345aa21387301858bbe1216b532f
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-07 11:59:43 -0500

    added add album

--- client/src/pages/AlbumDetails.js
+++ client/src/pages/AlbumDetails.js
@@ -1,24 +1,51 @@
 import { useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { GetAlbumDetails } from '../services/Album'
+import {
+  AddAlbumToDb,
+  GetAlbumDetails,
+  SearchAlbumsFromDb
+} from '../services/Album'
 
 const AlbumDetails = () => {
   const location = useLocation()
   const { album } = location.state
-  const [albumName, setAlbumName] = useState()
+  const [albumResult, setAlbumResult] = useState()
+  let artistName = album.artist.name.replace(/\s+/g, '+').replace(/\//g, '-')
+  let albumName = album.albumName.replace(/\s+/g, '+').replace(/\//g, '-')
+
+  const GetAlbumInfo = async (artistName, albumName) => {
+    let albumResult = await GetAlbumDetails(artistName, albumName)
+    // console.log(albumResult)
+  }
+  const checkAlbumExists = async (artistName, albumName) => {
+    let result = await SearchAlbumsFromDb(artistName, albumName)
+    console.log(result)
+    if (result) {
+      return
+    } else {
+      // let res = await AddAlbumToDb({
+      //   name: albumName,
+      //   artist: artistName,
+      //   image: album.large_image_url['#text'],
+      //   releaseDate: '1999'
+      // })
+      // console.log(res)
+    }
+  }
 
   useEffect(() => {
-    const GetAlbumDetails = async () => {}
-
-    const addAlbum = async (album) => {
-      let result = await GetAlbumDetails({
-        name: album.albumName,
-        artist: album.artist.name,
-        image: album.large_image_url,
-        releaseDate: '1999'
-      })
-    }
-    addAlbum(album)
+    // const addAlbum = async (album) => {
+    //   let result = await AddAlbumToDb({
+    //     name: album.albumName,
+    //     artist: album.artist.name,
+    //     image: album.large_image_url,
+    //     releaseDate: '1999'
+    //   })
+    //   console.log(result)
+    // }
+    // addAlbum(album)
+    // GetAlbumInfo(artistName, albumName)
+    checkAlbumExists(artistName, albumName)
   }, [])
 
   return (
