8c9c6d0603d1fa792b97bd30d4546040aef2d40d
Noah Vaughn <noahdvaughn@gmail.com>
2023-03-07 21:39:22 -0500

    changed album styles

--- client/src/components/AlbumCard.jsx
+++ client/src/components/AlbumCard.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 const AlbumCard = ({album}) => {
   return (
-  <Link to={`/album/${album.albumName.replace(/\s+/g, '+').replace(/\//g, "-")}`} state={{album:album}}>
-  
-  <div className="albumCard">
-  <p>{album.albumName}</p>
-  <p>{album.artist.name}</p>
-  <img src={album.medium_image_url['#text']} />
-</div>
-  
+  <Link className="albumCard" to={`/album/${album.albumName.replace(/\s+/g, '+').replace(/\//g, "-")}`} state={{album:album}}>
+    <div className="cardText">
+  <p className="noDec">{album.albumName}</p>
+  <p className="noDec">{album.artist}</p>
+
+    </div>
+  <img className="card"src={album.medium_image_url['#text']} />
+
   </Link>)
 }
 export default AlbumCard
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -6,36 +6,35 @@ const Navbar = ({ user, handleLogOut }) => {
 
 //    IMPLEMENT BOTH OF THESE IN A TERNARY WHEN WE FINISH USER AUTH 
 
-  // if (user) {
-  //   userOptions = (
-  //     <nav>
-  //       <h3>Welcome {user.email}!</h3>
-  //       <Link to="/feed">Feed</Link>
-  //       <Link onClick={handleLogOut} to="/">
-  //         Sign Out
-  //       </Link>
-  //     </nav>
-  //   )
-  // }
-  // const publicOptions = (
-  //   <nav>
-  //     <Link to="/">Home</Link>
-  //     <Link to="/register">Register</Link>
-  //     <Link to="/signin">Sign In</Link>
-  //   </nav>
-  // )
+  if (user) {
+    userOptions = (<header>
 
-
-
-
-  return (<header>
-      {/* {user ? userOptions : publicOptions} */}
-    <Link to='/' className='siteLogo'><img className='homeLogo'src='https://cdn-icons-png.flaticon.com/512/181/181156.png'/><p>TurnTables</p></Link>
-    <Link to='/feed'>Top Albums</Link>
+      <Link to='/' className='siteLogo'><img className='homeLogo'src='https://cdn-icons-png.flaticon.com/512/181/181156.png'/><p>TurnTables</p></Link>
+    <Link className='noDec'to='/feed'>Top Albums</Link>
     <Searchbar/>
-    
-    <Link to='/login'>Login</Link>
-    <Link to='/register'>Register</Link>
-  </header>)
+        <h3>Welcome {user.email}!</h3>
+        <Link className='noDec'onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+    </header>
+    )
+  }
+  const publicOptions = (<header>
+
+    <Link to='/' className='siteLogo'><img className='homeLogo'src='https://cdn-icons-png.flaticon.com/512/181/181156.png'/><p>TurnTables</p></Link>
+    <Link to='/feed' className='noDec'>Top Albums</Link>
+    <Searchbar/>
+    <Link className='navLink' to='/login'>Login</Link>
+    <Link className='navLink' to='/register'>Register</Link>
+  </header>
+  )
+
+
+
+
+  return <div>
+    {user ? userOptions :  publicOptions}
+
+  </div>
 }
 export default Navbar
